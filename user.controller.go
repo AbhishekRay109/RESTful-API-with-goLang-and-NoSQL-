829d3e76482f5f6968085caf67ff930090941da6
Abhishek Ray <86779456+AbhishekRay109@users.noreply.github.com>
2022-08-17 07:51:24 +0530

    Add files via upload

---
+++ user.controller.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserController struct {
+	UserService UserService
+}
+
+func New(userservice UserService) UserController {
+	return UserController{
+		UserService: userservice,
+	}
+}
+
+func (uc *UserController) CreateUser(ctx *gin.Context) {
+	var user User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	// Error handling for the create function and then calling it to assign the objects
+
+	err := uc.UserService.CreateUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+func (uc *UserController) GetUser(ctx *gin.Context) {
+	var username string = ctx.Param("name")
+	user, err := uc.UserService.GetUser(&username)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+}
+
+func (uc *UserController) GetAll(ctx *gin.Context) {
+	users, err := uc.UserService.GetAll()
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, users)
+}
+
+func (uc *UserController) UpdateUser(ctx *gin.Context) {
+	var user User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	err := uc.UserService.UpdateUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+func (uc *UserController) DeleteUser(ctx *gin.Context) {
+	var username string = ctx.Param("name")
+	err := uc.UserService.DeleteUser(&username)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+func (uc *UserController) RegisterUserRoutes(rg *gin.RouterGroup) {
+	userroute := rg.Group("/user")
+	userroute.POST("/create", uc.CreateUser)
+	userroute.GET("/get/:name", uc.GetUser)
+	userroute.GET("/getall", uc.GetAll)
+	userroute.PATCH("/update", uc.UpdateUser)
+	userroute.DELETE("/delete/:name", uc.DeleteUser)
+}
